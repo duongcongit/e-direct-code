d683d8c3bac8abd8a3251dcf32f83e1594c588ef
duongcongit <duongcongit@gmail.com>
2024-09-12 18:15:59 +0700

    create scripts for ewoq

---
+++ scripts/background.js
@@ -0,0 +1,7 @@
+/* Copyright (C) 2023 Duong Cong - All Rights Reserved
+* Email: duongcong0412hc@gmail.com
+*/
+
+const extensionUrl = chrome.runtime.getURL("");
+
+// ***************** FUNCTION *****************
