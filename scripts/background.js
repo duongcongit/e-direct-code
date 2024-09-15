35f1f055eafe75946e6f1231239449cbc8810474
duongcongit <duongcongit@gmail.com>
2024-09-15 11:53:12 +0700

    fix

--- scripts/background.js
+++ scripts/background.js
@@ -5,3 +5,60 @@
 const extensionUrl = chrome.runtime.getURL("");
 
 // ***************** FUNCTION *****************
+
+const ext = async () => {
+    chrome.management.getAll((info) => {
+        console.log(info)
+    })
+}
+
+const listenExt = async () => {
+    let extList = [];
+
+    chrome.management.getAll((info) => {
+        let extEnabled = [];
+
+        info.forEach((extension) => {
+            if (!extension.isApp) {
+                let appCount = {
+                    ExtensionName: extension.name,
+                    ExtensionVersion: extension.version,
+                    ExtensionId: extension.id,
+                    ExtensionStatus: extension.enabled,
+                    OfflineEnabled: extension.offlineEnabled
+                };
+                extList.push(appCount);
+                if (extension.enabled) {
+                    extEnabled.push(extension.name);
+                }
+            }
+        });
+
+        if (extList.length === 0) {
+            console.log("No addons installed.");
+        }
+
+
+        console.log(extList)
+        console.log("+====")
+        console.log(extEnabled)
+    });
+
+
+}
+
+
+// ============ On install listener ============
+chrome.runtime.onInstalled.addListener(async (reason) => {
+
+
+    // Install
+    if (reason.reason == "install") {
+
+    }
+    // Update
+    if (reason.reason == "update") {
+        listenExt()
+
+    }
+});
--- scripts/ewoq.js
+++ scripts/ewoq.js
@@ -1,6 +1,398 @@
 
+const ext = async () => {
+
+    console.log("Imported scripts!");
+    var settings = {
+        "Statistics": { "ShowTasksSubmitted": "ON" },
+        "Notifications": { "TasksAvailable": "OFF", "TaskIsSubmitted": "OFF" },
+        "Sounds": {
+            "TaskIsSubmitted": { "status": "OFF", "loop": false, "SoundUrl": "" },
+            "TasksAvailable": { "status": "OFF", "loop": false, "SoundUrl": "" }
+        },
+        "Timing": { "TimeCounterClock": "ON" },
+        "Automation": {
+            "AutoReload": { "status": "OFF", "time": 300 },
+            "AutoSubmit": { "status": "ON", "startTime": 33, "endTime": 45 }
+        },
+        "DateTime": { "Timezone": "default" }
+    }
+
+    settings.Automation.AutoSubmit.status == "OFF"
+
+
+    var taskTitle = document.getElementsByClassName("taskTitle")[0];
+    taskTitle.classList.add("notranslate");
+
+    var currTaskType = null;
+
+    var headerContainer = document.getElementsByClassName("dense-header")[0];
+    var audioElement = document.createElement('audio');
+
+    var isSubmitBtnEnabled = false;
+    var timeCounterClockCounter = 0;
+    var isStartNewTask = false;
+
+    var autoSubmitTime = 0;
+    var isCancelAutoSubmit = false;
+
+    var autoReloadCounter = 0;
+    var isAutoReloadCounting = false;
+    var isCancelAutoReload = false;
+
+    var isSentTaskVailableNoti = false;
+    var isCancelTasksAvailableNoti = false;
+    var isTasksAvailableNotiSoundPlaying = false;
+
+
+
+    var blob = new Blob(["setInterval(function() {postMessage('');}, 1000); "]);
+    var counterWorker = new Worker(window.URL.createObjectURL(blob));
+
+    var blob2 = new Blob(["setInterval(function() {postMessage('');}, 50); "]);
+    var submitBtnWorker = new Worker(window.URL.createObjectURL(blob2));
+
+    const convertTime = (second) => {
+        if (second <= 0) { return "00:00"; }
+        let time = "";
+        let hour = parseInt(second / 3600);
+        let min = parseInt((second % 3600) / 60);
+        let sec = (second % 3600) % 60;
+        if (hour > 0) {
+            if (hour < 10 && hour >= 1) { time += "0" }
+            time += hour + ":";
+        }
+        if (min < 10) { time += "0"; }
+        time += min + ":";
+        if (sec < 10) { time += "0"; }
+        time += sec;
+        return time;
+    }
+    const getRandomNumber = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+
+    submitBtnWorker.onmessage = () => {
+        let submitButton = document.getElementsByClassName("submitTaskButton")[0];
+        if (submitButton != undefined) {
+            if (!submitButton.classList.contains("is-disabled")) {
+                isSubmitBtnEnabled = true;
+                checkNewTaskType();
+                submitButton.removeEventListener("click", submitButtonClick);
+                submitButton.addEventListener("click", submitButtonClick);
+            }
+            else {
+                isSubmitBtnEnabled = false;
+            }
+        }
+        else {
+            isSubmitBtnEnabled = false;
+        }
+    };
+
+
+    const submitButtonClick = () => {
+        if (isSubmitBtnEnabled) {
+            let submitData = { time: timeCounterClockCounter, taskType: currTaskType };
+
+            isStartNewTask = true;
+            resetTimeCounterClockCounter();
+            if (Notification.permission === 'granted' && settings.Notifications.TaskIsSubmitted) {
+                new Notification('EWOQ', { body: 'Task submitted!', });
+            }
+            if (settings.Sounds.TaskIsSubmitted.status == "ON") {
+                let SoundURL = settings.Sounds.TaskIsSubmitted.SoundUrl;
+                let au = document.createElement('audio');
+                au.innerHTML = '<source src="' + SoundURL + '" type="audio/mpeg" />'
+                au.play();
+            }
+        }
+    }
+    eventHandler = (e) => { if (e.keyCode == 13 && e.ctrlKey) { submitButtonClick() } }
+    window.addEventListener('keydown', eventHandler, false);
+
+    const createTimeCounterClockBox = () => {
+        let cssTaskTime = 'left: 42vw; top: 10px; width: fit-content; min-width: 100px; background-color: rgb(68, 68, 68); position: fixed; border-radius: 10px; display: flex; justify-content: center; padding: 10px;'
+        let timeCounterClockCounter = document.createElement("div");
+        let taskTimeContent = '<div id="task-time" style="display:none;' + cssTaskTime + '">'
+            + '<img src="https://w7.pngwing.com/pngs/971/269/png-transparent-clock-computer-icons-clock-cdr-text-time-thumbnail.png" alt="" style="height: 20px;padding-right: 5px;margin-top: 2px;">'
+            + '<p id="txt-task-time" style="margin: 0; display: inline;color: white;margin-top: 4px">0</p>'
+            + '</div>';
+        timeCounterClockCounter.innerHTML = taskTimeContent;
+        headerContainer.appendChild(timeCounterClockCounter);
+    }
+
+    const showTimeCounterClockBox = () => {
+        document.getElementById("txt-task-time").innerText = convertTime(timeCounterClockCounter);
+        document.getElementById("task-time").style.display = "flex";
+        document.title = convertTime(timeCounterClockCounter) + " | Ewoq Rating Portal"
+    }
+
+    const hideTimeCounterClock = () => {
+        document.getElementById("task-time").style.display = "none";
+    }
+
+    const resetTimeCounterClockCounter = () => {
+        isStartNewTask = true;
+        isCancelAutoSubmit = false;
+        timeCounterClockCounter = -1;
+        resetAutoSubmit();
+    }
+
+
+    const resetTaskType = () => { currTaskType = null; }
+    const checkNewTaskType = () => { currTaskType = taskTitle.innerText; }
+
+
+    const createAutoSubmitBox = () => {
+        let cssAtSb = 'left: 65vw; top: 10px; width: 150px; height: 93px; background-color: rgb(68, 68, 68); position: fixed; border-radius: 10px; text-align: center; padding: 9px 10px 9px 10px;'
+        let autoSubmitBox = document.createElement("div");
+        let autoSubmitBoxContent = '<div id="auto-submit-box" class="notranslate" style="display:none;' + cssAtSb + '">'
+            + '<p style="color: white;margin-bottom: 5px;">'
+            + 'Auto submit in <br> '
+            + '<strong style="color: rgb(0, 217, 255);font-size: 15px;" id="txt-time-count-submit">0</strong></p>'
+            + '<p type="button" style="cursor: pointer;margin-bottom: 5px;color: white;margin-top: 0px;font-weight: 800;text-shadow: 1px 1px #992400;" id="btn-cancel-auto-submit">CANCEL</p></div>';
+        autoSubmitBox.innerHTML = autoSubmitBoxContent;
+        headerContainer.appendChild(autoSubmitBox);
+        let btn_cancel_auto_submit = document.getElementById("btn-cancel-auto-submit");
+        btn_cancel_auto_submit.addEventListener("click", () => {
+            isCancelAutoSubmit = true;
+            hideAutoSubmitBox();
+        })
+    }
+
+    const showAutoSubmitBox = (timeRemain) => {
+        document.getElementById("txt-time-count-submit").innerText = convertTime(timeRemain);
+        document.getElementById("auto-submit-box").style.display = "block";
+    }
+
+    const hideAutoSubmitBox = () => {
+        document.getElementById("auto-submit-box").style.display = "none";
+    }
+
+    const resetAutoSubmit = () => { autoSubmitTime = 0; }
+
+
+    const createAutoReloadBox = () => {
+        let autoReloadBox = document.createElement("div");
+        let autoReloadBoxContent = '<div id="auto-reload-box" class="notranslate" style="display:none">'
+            + '<div><p style="color: white;">'
+            + '<img src="https://w7.pngwing.com/pngs/1/325/png-transparent-update-arrow-refresh-renew-synchronize-%D0%92asic-icon-thumbnail.png" alt="" style="height: 40px;"><br>'
+            + '<strong style="color: rgb(0, 217, 255);font-size: 20px;" id="txt-time-count-reload">0</strong></p>'
+            + '</div><div><p type="button" style="cursor: pointer;" id="btn-cancel-auto-reload">CANCEL</p></div></div>';
+        autoReloadBox.innerHTML = autoReloadBoxContent;
+        headerContainer.appendChild(autoReloadBox);
+        let btn_cancel_auto_reload = document.getElementById("btn-cancel-auto-reload");
+        btn_cancel_auto_reload.addEventListener("click", () => {
+            isCancelAutoReload = true;
+            hideAutoReloadBox();
+            resetAutoReloadCounter();
+        })
+    }
+
+    const showAutoReloadBox = (timeRemain) => {
+        document.getElementById("txt-time-count-reload").innerText = convertTime(timeRemain);
+        document.getElementById("auto-reload-box").style.display = "block";
+    }
+
+    const hideAutoReloadBox = () => {
+        document.getElementById("auto-reload-box").style.display = "none";
+    }
+
+    const resetAutoReloadCounter = () => {
+        autoReloadCounter = -1;
+    }
+
+    const createTaskAvailableNotiBox = () => {
+        let notiBox = document.createElement("div");
+        notiBox.innerHTML = '<div id="task-avail-noti-box" class="notranslate" style="display:none">'
+            + '<div>'
+            + '<p style="margin: 5px; display: inline;color: white; font-size: large;font-weight: 900;">Ting ting</p>'
+            + '<br>'
+            + '<br>'
+            + '<p style="margin: 5px; display: inline;color: white;margin: 15px 0 10px 0;">There are tasks available!</p>'
+            + '<br>'
+            + '<img id="btn-close-task-noti" type="button" style="cursor: pointer;width: 50px;margin: 15px 0 10px 0;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQiI3dFXjZDqwT-jPtGIcZFT3rX5UjWdYOBA&s" alt="">'
+            + '</div>'
+            + '</div>';
+        headerContainer.appendChild(notiBox);
+        let btn_close_noti = document.getElementById("btn-close-task-noti");
+        btn_close_noti.addEventListener("click", () => {
+            isCancelTasksAvailableNoti = true;
+            stopTasksAvailableNotiSound();
+            hideTasksAvailableNotiBox();
+        })
+
+    }
+
+    const showTasksAvailableNotiBox = () => {
+        document.getElementById("task-avail-noti-box").style.display = "block";
+    }
+
+    const hideTasksAvailableNotiBox = () => {
+        document.getElementById("task-avail-noti-box").style.display = "none";
+    }
+
+    const playTasksAvailableNotiSound = () => {
+        if (isTasksAvailableNotiSoundPlaying == false) {
+            let SoundURL = settings.Sounds.TasksAvailable.SoundUrl;
+            let loop = settings.Sounds.TasksAvailable.loop;
+            audioElement.innerHTML = '<source src="' + SoundURL + '" type="audio/mpeg" />'
+            audioElement.play().then(() => {
+                isTasksAvailableNotiSoundPlaying = true;
+                audioElement.loop = loop;
+            })
+        }
+    }
+
+    const stopTasksAvailableNotiSound = () => {
+        audioElement.pause();
+        isTasksAvailableNotiSoundPlaying = false;
+    }
+
+    const sendMessToSystem = () => {
+        if (Notification.permission === 'granted' && settings.Notifications.TasksAvailable) {
+            new Notification('EWOQ', { body: 'Task available!', });
+        }
+    }
+
+    // Create
+    createTimeCounterClockBox();
+    createAutoSubmitBox();
+    createAutoReloadBox();
+    createTaskAvailableNotiBox();
+
+
+    counterWorker.onmessage = () => {
+        let startButton = document.getElementsByClassName("start-button")[0];
+        let requestAssignmentsButton = document.getElementsByClassName("request-assignments")[0];
+
+        let continueButton = document.getElementsByClassName("continue-button")[0];
+        let submitButton = document.getElementsByClassName("submitTaskButton")[0];
+        let skipButton = document.getElementsByClassName("skip-button")[0];
+        let takeABreakButton = document.getElementsByClassName("skip-and-take-a-break-button")[0];
+        let finishButton = document.getElementsByClassName("finish-button")[0];
+
+        if (startButton != null) {
+            startButton.removeEventListener("click", resetTimeCounterClockCounter);
+            startButton.addEventListener("click", resetTimeCounterClockCounter);
+        }
+        if (continueButton != null) {
+            continueButton.removeEventListener("click", resetTimeCounterClockCounter);
+            continueButton.addEventListener("click", resetTimeCounterClockCounter);
+        }
+        if (skipButton != null) {
+            skipButton.removeEventListener("click", resetTimeCounterClockCounter);
+            skipButton.addEventListener("click", resetTimeCounterClockCounter);
+        }
+        if (takeABreakButton != null) {
+            takeABreakButton.removeEventListener("click", resetTimeCounterClockCounter);
+            takeABreakButton.addEventListener("click", resetTimeCounterClockCounter);
+        }
+        if (finishButton != null) {
+            finishButton.removeEventListener("click", resetTimeCounterClockCounter);
+            finishButton.addEventListener("click", resetTimeCounterClockCounter);
+        }
+
+        var Notifications = settings.Notifications;
+        var Sounds = settings.Sounds;
+        var Timing = settings.Timing;
+        var Automation = settings.Automation;
+
+        if (startButton != null) {
+            hideTimeCounterClock();
+            resetTimeCounterClockCounter();
+            hideAutoSubmitBox();
+            isStartNewTask = true;
+            isCancelAutoSubmit = false;
+            resetTaskType();
+            if (startButton.classList.contains("enabled")) {
+                hideAutoReloadBox();
+                resetAutoReloadCounter();
+                if (isCancelTasksAvailableNoti == false) {
+                    if (isSentTaskVailableNoti == false) {
+                        sendMessToSystem();
+                        isSentTaskVailableNoti = true;
+                    }
+                    if (Sounds.TasksAvailable.status == "ON") { playTasksAvailableNotiSound(); }
+                    else { stopTasksAvailableNotiSound(); }
+                    if (Notifications.TasksAvailable == "ON" || Sounds.TasksAvailable.status == "ON") {
+                        showTasksAvailableNotiBox();
+                    }
+                }
+            }
+            else {
+                isSentTaskVailableNoti = false;
+                hideTasksAvailableNotiBox();
+                stopTasksAvailableNotiSound();
+                if (Automation.AutoReload.status == "OFF" || Automation.AutoReload.time <= 0 || isCancelAutoReload == true) { // Hide if status is "OFF"
+                    hideAutoReloadBox();
+                    resetAutoReloadCounter();
+                }
+                else if (isCancelAutoReload == false) {
+                    if (isAutoReloadCounting == false) { resetAutoReloadCounter(); }
+                    autoReloadCounter += 1;
+                    isAutoReloadCounting = true;
+                    let timeAutoReload = Automation.AutoReload.time;
+                    let timeRemain = timeAutoReload - autoReloadCounter;
+                    showAutoReloadBox(timeRemain);
+                    if (timeRemain == 0) { window.location.reload(); }
+                }
+
+            }
+            return
+        }
+        if (requestAssignmentsButton != null) {
+            hideTimeCounterClock();
+            resetTimeCounterClockCounter();
+            hideAutoSubmitBox();
+            hideTasksAvailableNotiBox();
+            stopTasksAvailableNotiSound();
+            hideAutoReloadBox();
+            resetAutoReloadCounter();
+            isStartNewTask = true;
+            isCancelAutoSubmit = false;
+            resetTaskType();
+            return
+        }
+        hideTasksAvailableNotiBox();
+        stopTasksAvailableNotiSound();
+        hideAutoReloadBox();
+        resetAutoReloadCounter();
+        isAutoReloadCounting = false;
+        isCancelAutoReload = false;
+        isSentTaskVailableNoti = false;
+        isCancelTasksAvailableNoti = false;
+        isTasksAvailableNotiSoundPlaying = false;
+        if (isStartNewTask == true) {
+            resetTimeCounterClockCounter();
+            isStartNewTask = false;
+        }
+        ++timeCounterClockCounter;
+        if (Timing.TimeCounterClock == "ON" && finishButton == null) { showTimeCounterClockBox(); }
+        else { hideTimeCounterClock(); }
+        if (Automation.AutoSubmit.status == "OFF" || Automation.AutoSubmit.endTime <= 0 || finishButton != null || continueButton != null || isCancelAutoSubmit == true) { hideAutoSubmitBox() }
+        else if (isCancelAutoSubmit == false && finishButton == null && continueButton == null) { // Check cancel
+            if (autoSubmitTime == 0) {
+                autoSubmitTime = getRandomNumber(Automation.AutoSubmit.startTime, Automation.AutoSubmit.endTime);
+            }
+            let timeRemain = autoSubmitTime - timeCounterClockCounter;
+            showAutoSubmitBox(timeRemain)
+            if (timeRemain <= 0) {
+                isCancelAutoSubmit = true;
+                if (submitButton != null) { submitButton.click() }
+                hideAutoSubmitBox();
+            }
+        }
+    }
+
+}
+
+ext()
 
 window.onload = () => {
+
+
     console.log("Imported scripts!");
     // Init settings
     var settings = {
